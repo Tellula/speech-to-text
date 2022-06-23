5f04701464a87bf79569e4e316a3ba648bce08f6
Tellula <julieloane@gmail.com>
2022-06-23 17:25:59 +0200

    First commit remove key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import MicRecorder from "mic-recorder-to-mp3";
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
 
-function App() {
+// Set AssemblyAI Axios Header
+const assembly = axios.create({
+  baseURL: "https://api.assemblyai.com/v2",
+  headers: {
+    authorization: process.env.REACT_APP_API_KEY,
+    "content-type": "application/json",
+  },
+});
+
+const App = () => {
+  // Mic-Recorder-To-MP3
+  const recorder = useRef(null); //Recorder
+  const audioPlayer = useRef(null); //Ref for the HTML Audio Tag
+  const [blobURL, setBlobUrl] = useState(null);
+  const [audioFile, setAudioFile] = useState(null);
+  const [isRecording, setIsRecording] = useState(null);
+
+  useEffect(() => {
+    //Declares the recorder object and stores it inside of ref
+    recorder.current = new MicRecorder({ bitRate: 128 });
+  }, []);
+
+  const startRecording = () => {
+    // Check if recording isn't blocked by browser
+    recorder.current.start().then(() => {
+      setIsRecording(true);
+    });
+  };
+
+  const stopRecording = () => {
+    recorder.current
+      .stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        const file = new File(buffer, "audio.mp3", {
+          type: blob.type,
+          lastModified: Date.now(),
+        });
+        const newBlobUrl = URL.createObjectURL(blob);
+        setBlobUrl(newBlobUrl);
+        setIsRecording(false);
+        setAudioFile(file);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  // AssemblyAI API
+
+  // State variables
+  const [uploadURL, setUploadURL] = useState("");
+  const [transcriptID, setTranscriptID] = useState("");
+  const [transcriptData, setTranscriptData] = useState("");
+  const [transcript, setTranscript] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Upload the Audio File and retrieve the Upload URL
+  useEffect(() => {
+    if (audioFile) {
+      assembly
+        .post("/upload", audioFile)
+        .then((res) => setUploadURL(res.data.upload_url))
+        .catch((err) => console.error(err));
+    }
+  }, [audioFile]);
+
+  // Submit the Upload URL to AssemblyAI and retrieve the Transcript ID
+  const submitTranscriptionHandler = () => {
+    assembly
+      .post("/transcript", {
+        audio_url: uploadURL,
+        language_detection: true,
+      })
+      .then((res) => {
+        setTranscriptID(res.data.id);
+
+        checkStatusHandler();
+      })
+      .catch((err) => console.error(err));
+  };
+
+  // Check the status of the Transcript
+  const checkStatusHandler = async () => {
+    setIsLoading(true);
+    try {
+      await assembly.get(`/transcript/${transcriptID}`).then((res) => {
+        setTranscriptData(res.data);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // Periodically check the status of the Transcript
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (transcriptData.status !== "completed" && isLoading) {
+        checkStatusHandler();
+      } else {
+        setIsLoading(false);
+        setTranscript(transcriptData.text);
+
+        clearInterval(interval);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <audio ref={audioPlayer} src={blobURL} controls="controls" />
+      <div>
+        <button disabled={isRecording} onClick={startRecording}>
+          Start
+        </button>
+        <button disabled={!isRecording} onClick={stopRecording}>
+          Stop
+        </button>
+        <button onClick={submitTranscriptionHandler}>Submit</button>
+      </div>
+      {transcriptData.status === "completed" ? (
+        <p>{transcript}</p>
+      ) : (
+        <p>{transcriptData.status}...</p>
+      )}
+      <button onClick={refreshPage}>RESET</button>
     </div>
   );
-}
+};
 
 export default App;
